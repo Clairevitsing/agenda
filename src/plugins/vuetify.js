257c7dde9e0677d2cadbf04f8cd487ea1cfd5d86
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-02-23 15:56:02 +0100

    first commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+// Styles
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
